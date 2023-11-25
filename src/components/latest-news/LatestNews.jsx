df1f4d79f8946fbbbd2665097b8a08892cd8c606
deepak9875 <deepaklakhlan1234@gmail.com>
2023-11-25 22:46:45 +0530

    chnage btn css ini latest news section

--- src/components/latest-news/LatestNews.jsx
+++ src/components/latest-news/LatestNews.jsx
@@ -53,7 +53,7 @@ const LatestNews = () => {
               open-source Java runtime binaries that are enterprise-ready and
               Java SE TCK-tested for general use in the Java ecosystem.
             </h3>
-            <button className="rounded-2xl bg-transparent gradient-border border-2 border-pink-500/0 text-white text-base leading-6 font-bold py-3 px-8 block w-fit">
+            <button className="rounded-2xl bg-transparent gradient-border border-2 border-pink-500/0 text-white text-base leading-6 font-bold w-[154px] h-[48px] block ">
               See all news
             </button>
           </div>
