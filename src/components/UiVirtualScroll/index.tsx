c9132dd12807fe26aaa6aa9af0350ab725f27dd4
shivc8658 <shiv.kumar@tasolglobal.com>
2023-11-25 20:36:32 +0530

    mobile view fixed

--- src/components/UiVirtualScroll/index.tsx
+++ src/components/UiVirtualScroll/index.tsx
@@ -28,49 +28,6 @@ const data = [
 const UiVirtualScroll = () => {
   return (
     <>
-      {/* <div className="mx-auto max-w-5xl items-center">
-        <div
-          className="hidden lg:flex scroll-container h-screen m-12"
-          ref={scrollContainerRef}
-        >
-          <div className="flex flex-col h-23 ml-8 flex-1 p-4">
-            {data.map((item, index) => {
-              const image = require(`../../images/${item.image}`)
-              return (
-                <img
-                  src={image.default}
-                  alt="Description"
-                  className={`w-full h-1/3 object-cover mb-4 scroll-item ${
-                    index === 0 ? "active" : ""
-                  }`}
-                />
-              )
-            })}
-          </div>
-
-          <div className="md:w-12 flex-none p-4 relative">
-            <div className="progress absolute top-4 bottom-4 left-0 right-0">
-              <div className="progress-fill" ref={progressFillRef}></div>
-            </div>
-            <div className="progress-dot" ref={progressDotRef}></div>
-          </div>
-          <div className="flex flex-col ml-8 flex-1 p-4">
-            {data.map((item, index) => (
-              <div
-                className={`flex flex-col justify-center flex-1 text-scroll-item scroll-item ${
-                  index === 0 ? "active" : ""
-                }`}
-                key={index}
-              >
-                <h1 className="text-xl md-pt-6 my-5 font-bold">{item.title}</h1>
-                <p className="text-base text-white-600">{item.description}</p>
-              </div>
-            ))}
-          </div>
-        </div>
-
-        <UiMobileScroll data={data} />
-      </div> */}
       <UiMobileScroll data={data} />
       <div className="lg:block hidden open-animation-wrapper">
         <UiVirtualContent data={data} />
--- src/components/UiVirtualScroll/mobile.tsx
+++ src/components/UiVirtualScroll/mobile.tsx
@@ -1,33 +1,27 @@
-import React from "react";
+import React from "react"
 
 // @ts-ignore
-import ScrollDivider from '../../images/scroll-divider.svg';
+import ScrollDivider from "../../images/scroll-divider.svg"
 
-const UiMobileScroll = ({data}) => {
+const UiMobileScroll = ({ data }) => {
+  return data.map((item, index) => {
+    const image = require(`../../images/${item.image}`)
     return (
-        data.map((item, index) => {
-            const image = require(`../../images/${item.image}`);
-            return (
-                // render an image in the center and then the text below it centered
-                <div className="lg:hidden flex flex-col text-center items-center">
-                    <img
-                        src={image.default}
-                        alt="Description"
-                        className="w-full h-1/3 object-cover"
-                    />
-                    <img src={ScrollDivider} alt='scroll-divider' />
-                    <div className="flex flex-col flex-1 p-4">
-                    <h1 className="text-xl my-5 font-bold">
-                        {item.title}
-                    </h1>
-                    <p className="text-base text-white-600">
-                        {item.description}
-                    </p>
-                    </div>
-                </div>
-            )
-        })
+      // render an image in the center and then the text below it centered
+      <div className="lg:hidden max-w-[500px] mx-auto flex flex-col text-center items-center">
+        <img
+          src={image.default}
+          alt="Description"
+          className="w-full mb-0 max-w-[366px] object-cover"
+        />
+        <img src={ScrollDivider} className="mb-0" alt="scroll-divider" />
+        <div className="flex flex-col flex-1 px-4">
+          <h1 className="text-xl my-5 font-bold">{item.title}</h1>
+          <p className="text-base text-[#C4BFCE]">{item.description}</p>
+        </div>
+      </div>
     )
+  })
 }
 
-export default UiMobileScroll;
+export default UiMobileScroll
