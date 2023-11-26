244a20ef13493cdf772aa77abfea4cfac91e4111
kumarsuneel162@gmail.com <kumarsuneel162@gmail.com>
2023-11-26 18:30:53 +0530

    open

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,6 +10,8 @@ import Testimonials from "../components/Testimonials"
 import LatestNews from "../components/latest-news/LatestNews"
 import { AppleIcon, HeroBgImg } from "../components/common/Icon"
 import Contributors from "../components/contributors/Contributors"
+import UiVirtualContent from "../components/UiVirtualScroll/UiVirtualContent"
+import OpenBased from "../components/open-based/OpenBased"
 
 const Index = () => {
   return (
@@ -56,6 +58,7 @@ const Index = () => {
       <div className="w-full h-[1px] my-8 lg:my-16 bg-[#3E3355]"></div>
       <PowerOfTemurin />
       <DownloadCounter />
+      <OpenBased />
       <Projects />
       <Testimonials />
       <LatestNews />
